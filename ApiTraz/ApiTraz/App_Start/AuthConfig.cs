d1b6d934f75511f2741c89833566a348ab3c966d
Marcela Cocco <coccomarcela@gmail.com>
2013-06-10 17:03:41 -0300

    holaaaa

---
+++ ApiTraz/ApiTraz/App_Start/AuthConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+using ApiTraz.Models;
+
+namespace ApiTraz
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            // Para permitir que los usuarios de este sitio inicien sesión con sus cuentas de otros sitios como, por ejemplo, Microsoft, Facebook y Twitter,
+            // es necesario actualizar este sitio. Para obtener más información, visite http://go.microsoft.com/fwlink/?LinkID=252166
+
+            //OAuthWebSecurity.RegisterMicrosoftClient(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //OAuthWebSecurity.RegisterTwitterClient(
+            //    consumerKey: "",
+            //    consumerSecret: "");
+
+            OAuthWebSecurity.RegisterFacebookClient(
+                appId: "322546007843467",
+                appSecret: "d962dfd6c5c28be4e0ce5f16011fdb59");
+
+            OAuthWebSecurity.RegisterGoogleClient();
+        }
+    }
+}
